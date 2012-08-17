110e11deeeb157d50572a6f6bb18c0ea94aa246a
Josmas <josmasflores@gmail.com>
2012-08-17 23:51:54 +0100

    Initial import. Hello World

---
+++ src/org/p2pu/app/MainActivity.java
@@ -0,0 +1,13 @@
+package org.p2pu.app;
+
+import android.os.Bundle;
+import org.apache.cordova.*;
+
+public class MainActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
