d15cdd8b16a420094734177f295e26e2643b430c
Josmas <josmasflores@gmail.com>
2012-08-18 22:18:48 +0100

    adds page with school details, and todo list

--- assets/www/schools.js
+++ assets/www/schools.js
@@ -1,25 +1,44 @@
 var serviceURL = "http://api.localhost:8000/api/alpha/school/";
 
-var schools;
-
 $('#mainPage').bind('pageinit', function(event) {
   getSchoolsList();
 });
 
 function getSchoolsList(){
   var call = $.getJSON(serviceURL + '?format=jsonp&callback=?', function(data) {
+    var schools;
     $('#schoolsList li').remove();
     
     schools = data.objects;
     $.each(schools, function(index, school) {
       $('#schoolsList').append(
-        '<li><a href="#">' +
+        '<li><a href="schooldetails.html?id=' + school.id + '">' +
         '<h3>' + school.name + '<h3/>' +
         '<h4>' + school.short_name + '</h4>' +
-        '<p>' + school.description + '</p>' +
         '</a></li>');
     });
     
     $('#schoolsList').listview('refresh');
   });
+}
+
+$('#schoolDetailsPage').live('pageshow', function(event) {
+  var id = getParams()["id"];
+  $.getJSON(serviceURL + id + '?format=jsonp&callback=?', showSchoolDetails);
+});
+
+function showSchoolDetails(data) {
+  school = data;
+  $('#description').html(school.description);
+}
+
+function getParams() {
+  var params = [], hash;
+  var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+  for(var i = 0, len = hashes.length; i < len; i++){
+    hash = hashes[i].split('=');
+    params.push(hash[0]);
+    params[hash[0]] = hash[1];
+  }
+  return params;
 }
